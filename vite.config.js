b42775ae95b2d0e75cce710af0e30cabaeeb33a8
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-10-06 17:09:10 +0200

    fixed mock axios file error

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
+    mockReset: true,
+    restoreMocks: true,
+    clearMocks: true,
   },
 });
