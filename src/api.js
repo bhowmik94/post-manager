7bfbf1a67c805895a2d39c1a4994d7524e1dcaab
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-10-07 12:22:52 +0200

    still trying to resolve CI vitest issue

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:5000/api"
 });
-
+console.log("REAL API LOADED");
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
